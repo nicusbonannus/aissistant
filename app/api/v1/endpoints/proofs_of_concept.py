8ba85f0209e5f228a671211a94e79c356ba2ed53
nicus <=>
2025-04-28 20:27:08 -0300

    Fix linter issues

--- app/api/v1/endpoints/proofs_of_concept.py
+++ app/api/v1/endpoints/proofs_of_concept.py
@@ -1,34 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from pydantic import BaseModel, Field
-from sqlalchemy.orm import Session
 from starlette import status
 
-from app.db import crud
-from app.db.database import get_db
-from app.db.schemas import UserCreate
 from app.services.llm_handler import LLMHandler
 
 router = APIRouter()
-
-
-class CreateUserInfo(BaseModel):
-    name: str = Field(...)
-    email: str = Field(...)
-
-
-class UserInformation(BaseModel):
-    id: int = Field(...)
-    name: str = Field(...)
-    email: str = Field(...)
-
-
-@router.post(
-    "/user", status_code=status.HTTP_201_CREATED, response_model=UserInformation
-)
-def create_user(user_info: CreateUserInfo, db: Session = Depends(get_db)):
-    user_create = UserCreate(name=user_info.name, email=user_info.email)
-    response = crud.create_user(db, user_create)
-    return UserInformation(name=response.name, email=response.email, id=response.id)
 
 
 class DiscountInfo(BaseModel):
