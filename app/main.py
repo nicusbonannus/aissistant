677fdd5d98ed169764c66e0cae6e32d28edc153d
nicus <=>
2025-04-28 20:24:11 -0300

    Add basic CORS to debug and test locally

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import router as api_router
 from app.core.config import settings
@@ -15,6 +16,13 @@ tags_metadata = [
 ]
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
